13e1794ba5c5155c241fb441222f108bfaa0e6c8
aabda2006 <aabda2006@gmail.com>
2026-01-19 12:21:18 +0100

    feat: test pipeline v2 - modification du titre

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -42,7 +42,7 @@ function App() {
 
   return (
     <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-      <h1>Mon App 3-Tiers : pipeline CI/CD complet</h1>
+      <h1>Mon App 3-Tiers : Test pipeline CI/CD complet #2</h1>
       <p><strong>Status :</strong> {status}</p>
       <hr />
       
